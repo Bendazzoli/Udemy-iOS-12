1a2d585bc2c80206b2428d6c96eda682480be077
Paulo Henrique Bendazzoli <Paulo.Bendazzoli-ext@ab-inbev.com>
2021-11-05 14:50:12 -0300

    Criando o layout do app AlcoolOuGasolina

---
+++ AlcoolOuGasolina/AlcoolOuGasolina/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlcoolOuGasolina
+//
+//  Created by Paulo Henrique Bendazzoli on 05/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var precoAlcool: UITextField!
+    @IBOutlet weak var precoGasolina: UITextField!
+    
+    @IBOutlet weak var resultado: UILabel!
+    
+    @IBAction func calcularPreco(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
