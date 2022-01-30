bba5273e8b73c777951f6c058abdf1e0ee8e275b
Paulo Henrique Bendazzoli <Paulo.Bendazzoli-ext@ab-inbev.com>
2022-01-30 13:37:42 -0300

    Criando ListaDeTarefas

---
+++ ListaDeTarefas/ListaDeTarefas/AdicionarTarefaViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AdicionarTarefaViewController.swift
+//  ListaDeTarefas
+//
+//  Created by Paulo Henrique Bendazzoli on 30/01/22.
+//
+
+import UIKit
+
+class AdicionarTarefaViewController: UIViewController {
+    
+    
+    @IBAction func addNovaTarefaButton(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ ListaDeTarefas/ListaDeTarefas/TableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  ListaDeTarefas
+//
+//  Created by Paulo Henrique Bendazzoli on 30/01/22.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    let goToAdicionarTarefaViewContrller = "goToAdicionarTarefaViewContrller"
+    
+    var listaTarefas: [String] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        listaTarefas.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let listaTarefasReuseTableViewCell = "listaTarefasReuseTableViewCell"
+        let listaTarefasCell = tableView.dequeueReusableCell(withIdentifier: listaTarefasReuseTableViewCell, for: indexPath)
+        listaTarefasCell.textLabel?.text = listaTarefas[indexPath.row]
+        return listaTarefasCell
+    }
+}
+
