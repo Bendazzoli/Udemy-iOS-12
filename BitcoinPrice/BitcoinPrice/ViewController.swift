a551f323069632811513d2186826847d9de68c03
Paulo Henrique Bendazzoli <Paulo.Bendazzoli-ext@ab-inbev.com>
2022-02-14 10:52:28 -0300

    Criando app para consultar preco Bitcoin via URLSession

---
+++ BitcoinPrice/BitcoinPrice/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  BitcoinPrice
+//
+//  Created by Paulo Henrique Bendazzoli on 14/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var btcPrice: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let configuration = URLSessionConfiguration.default
+        let session = URLSession(configuration: configuration)
+        
+        guard let url = URL(string: "https://api.coinbase.com/v2/prices/spot?currency=BRL") else {
+            fatalError()
+        }
+        
+        let task = session.dataTask(with: url) { data, response, error in
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200..<300).contains(httpResponse.statusCode) else {
+                return
+            }
+            
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                if let result = try JSONSerialization.jsonObject(with: data, options: [])as? [String: Any]{
+                    if let dataJson = result["data"] as? [String: String] {
+                        if let amount = dataJson["amount"] {
+                            print(Float(amount)!)
+                        }
+                    }
+                }
+            } catch {
+                fatalError()
+            }
+        }
+        
+        task.resume()
+    }
+}
